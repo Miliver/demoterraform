3b0fd04101d229ec78f436e407e7f5a190b3de2f
Milan Slovak <milan.slovak@thermofisher.com>
2024-02-12 18:34:11 +0100

    initial commit

---
+++ Controllers/HealthCheckController.fs
@@ -0,0 +1,18 @@
+﻿namespace DemoTerraform.Controllers
+
+open System
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open DemoTerraform
+
+[<ApiController>]
+[<Route("[controller]")>]
+type HealthCheckController (logger : ILogger<HealthCheckController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        {|
+            State = HealtCheckState.Ok
+            Running = DateTime.UtcNow - System.Diagnostics.Process.GetCurrentProcess().StartTime
+        |}
---
+++ HealthCheck.fs
@@ -0,0 +1,11 @@
+﻿namespace DemoTerraform
+
+open System
+
+type HealtCheckState =
+| Ok = 0
+| Nok = 1
+
+type HealtCheckReport =
+    { State: HealtCheckState
+      Running: TimeSpan }
